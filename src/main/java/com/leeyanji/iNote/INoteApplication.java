0ef43a752a29e698a445ac552617592fc8fa1a5d
leeyanji <@PAsswordGithu12345>
2019-05-13 09:31:15 +0700

    add sercurity

--- src/main/java/com/leeyanji/iNote/INoteApplication.java
+++ src/main/java/com/leeyanji/iNote/INoteApplication.java
@@ -5,11 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.leeyanji.iNote.repository")
@@ -18,12 +21,16 @@ public class INoteApplication extends WebSecurityConfigurerAdapter {
 	public static void main(String[] args) {
 		SpringApplication.run(INoteApplication.class, args);
 	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
-		// No login
-		http.authorizeRequests().antMatchers("/").permitAll();
-//		http.authorizeRequests().antMatchers("/admin").authenticated().and().formLogin();
+		http.authorizeRequests().antMatchers("/").hasRole("ADMIN").and().formLogin();
+
 	}
 
 	@Override
@@ -32,6 +39,6 @@ public class INoteApplication extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//		auth.inMemoryAuthentication().withUser("admin").password("12345").roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("leeyanji").password(passwordEncoder().encode("12345")).roles("ADMIN");
 	}
 }
---
+++ src/test/java/com/leeyanji/iNote/INoteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.leeyanji.iNote;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class INoteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
